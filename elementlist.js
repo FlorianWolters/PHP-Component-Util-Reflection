af1cd5343ceab33c72887d55a68b705e387ec23b
Florian Wolters <wolters.fl@gmail.com>
2013-03-01 12:59:23 +0100

    Updated API documentation.

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","FlorianWolters\\Component\\Util\\ReflectionUtils"]];
+ApiGen.elements = [["c","FlorianWolters\\Component\\Util\\ReflectionUtils"],["c","ReflectionClass"],["c","ReflectionFunctionAbstract"],["c","ReflectionMethod"],["c","Reflector"]];
