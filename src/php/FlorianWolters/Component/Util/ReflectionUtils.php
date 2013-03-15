9a4b7a7b958014c20ec6fb823440ea5d1a1b9aff
Florian Wolters <wolters.fl@gmail.com>
2013-03-15 18:33:02 +0100

    Added methods invokeMethod, methodsForClassWithoutInheritedMethods, methodsForClass and parentClassesForClass. Signed-off-by:Florian Wolters <wolters.fl@gmail.com>

--- src/php/FlorianWolters/Component/Util/ReflectionUtils.php
+++ src/php/FlorianWolters/Component/Util/ReflectionUtils.php
@@ -3,6 +3,7 @@ namespace FlorianWolters\Component\Util;
 
 use \ReflectionClass;
 use \ReflectionMethod;
+use \ReflectionObject;
 
 use FlorianWolters\Component\Core\StringUtils;
 
@@ -27,7 +28,7 @@ class ReflectionUtils
      *
      * Instead, the class should be used as:
      * /---code php
-     * $reflectedClass = ReflectionUtils::reflectClassUnderTest();
+     * ReflectionUtils::invokeMethodOnObject($obj, '__toString');
      * \---
      */
     protected function __construct()
@@ -37,18 +38,18 @@ class ReflectionUtils
     // @codeCoverageIgnoreEnd
 
     /**
-     * Creates a new instance for the specified class name without invoking the
-     * constructor.
+     * Creates a new instance of the class with the specified class name
+     * without invoking its constructor.
      *
-     * The constructor of the class is set accessible if it is not declared with
-     * `public` visibility.
+     * The constructor is set accessible if it is not declared with `public`
+     * visibility.
      *
-     * The method {@see createNewInstanceWithoutConstructor} accepts a variable
-     * number of parameters which are passed to the constructor of the class.
+     * The method `createNewInstanceWithoutConstructor` accepts a variable
+     * number of arguments which are passed to the constructor of the class.
      *
-     * @param string  $className  The name of the class to reflect.
-     * @param mixed[] $parameters Zero or more parameters to be passed to the
-     *                            constructor.
+     * @param string  $className The name of the class to reflect.
+     * @param mixed[] $arguments The optional arguments to be passed to the
+     *                           constructor.
      *
      * @return object A new instance of the class to reflect.
      *
@@ -57,7 +58,7 @@ class ReflectionUtils
      */
     public static function createNewInstanceWithoutConstructor(
         $className,
-        array $parameters = array()
+        array $arguments = []
     ) {
         $reflectedClass = new ReflectionClass($className);
         $newInstance = $reflectedClass->newInstanceWithoutConstructor();
@@ -65,17 +66,151 @@ class ReflectionUtils
 
         if (null !== $reflectedConstructor) {
             $reflectedConstructor->setAccessible(true);
-            $reflectedConstructor->invokeArgs($newInstance, $parameters);
+            $reflectedConstructor->invokeArgs($newInstance, $arguments);
         }
 
         return $newInstance;
     }
 
     /**
+     * Invokes the specified method on the specified object.
+     *
+     * The method `invokeMethod` accepts a variable number of arguments which
+     * are passed to the method of the object.
+     *
+     * @param object  $object     The object on which to invoke the method.
+     * @param strin   $methodName The name of the method to invoke.
+     * @param mixed[] $arguments  The optional arguments to be passed to the
+     *                            method.
+     *
+     * @return void
+     */
+    public static function invokeMethod(
+        $object,
+        $methodName,
+        array $arguments = []
+    ) {
+        $reflectedObject = new ReflectionObject($object);
+        $reflectedMethod = $reflectedObject->getMethod($methodName);
+        $reflectedMethod->setAccessible(true);
+        $reflectedMethod->invokeArgs($object, $arguments);
+    }
+
+    /**
+     * Returns all {@see ReflectionMethod} objects (excluding methods inherited
+     * from superclasses) for the specified class name.
+     *
+     * The `$filter` argument uses a logical AND to filter the methods.
+     *
+     * @param string    $className The name of the class to check.
+     * @param integer[] $filters   Any combination of
+     *                             `ReflectionMethod::IS_STATIC`,
+     *                             `ReflectionMethod::IS_PUBLIC`,
+     *                             `ReflectionMethod::IS_PROTECTED`,
+     *                             `ReflectionMethod::IS_PRIVATE`,
+     *                             `ReflectionMethod::IS_ABSTRACT`,
+     *                             `ReflectionMethod::IS_FINAL`.
+     *
+     * @return ReflectionMethod[] The {@see ReflectionMethod} objects reflecting
+     *                            each method.
+     *
+     * @throw ReflectionException If the class with the specified class name
+     *                            does not exist.
+     */
+    public static function methodsForClassWithoutInheritedMethods(
+        $className,
+        array $filters = []
+    ) {
+        $result = [];
+
+        $methods = self::methodsForClass(
+            $className,
+            $filters
+        );
+
+        /* @var $method ReflectionMethod */
+        foreach ($methods as $method) {
+            if ($method->class === $className) {
+                $result[] = $method;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Returns all {@see ReflectionMethod} objects (inclduign methods inherited
+     * from superclasses) for the specified class name.
+     *
+     * The `$filter` argument uses a logical AND to filter the methods.
+     *
+     * @param string    $className The name of the class to check.
+     * @param integer[] $filters   Any combination of
+     *                             `ReflectionMethod::IS_STATIC`,
+     *                             `ReflectionMethod::IS_PUBLIC`,
+     *                             `ReflectionMethod::IS_PROTECTED`,
+     *                             `ReflectionMethod::IS_PRIVATE`,
+     *                             `ReflectionMethod::IS_ABSTRACT`,
+     *                             `ReflectionMethod::IS_FINAL`.
+     * @return ReflectionMethod[] The {@see ReflectionMethod} objects reflecting
+     *                            each method.
+     *
+     * @throw ReflectionException If the class with the specified class name
+     *                            does not exist.
+     */
+    public static function methodsForClass(
+        $className,
+        array $filters = []
+    ) {
+        $result = [];
+        $reflectedClass = new ReflectionClass($className);
+
+        if (true === empty($filters)) {
+            $result = $reflectedClass->getMethods();
+        } else {
+            $result = $reflectedClass->getMethods($filters[0]);
+
+            for ($i = 1; $i < count($filters); ++$i) {
+                $result = \array_intersect(
+                    $result,
+                    $reflectedClass->getMethods($filters[$i])
+                );
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Returns all {@see ReflectionClass} objects for the parent classes of the
+     * specified class name.
+     *
+     * @param string $className The name of the class to check.
+     *
+     * @return ReflectionClass[] The {@see ReflectionClass} objects reflecting
+     *                           each parent class.
+     *
+     * @throw ReflectionException If the class with the specified class name
+     *                            does not exist.
+     */
+    public static function parentClassesForClass($className)
+    {
+        $result = [];
+        $reflectedClass = new ReflectionClass($className);
+
+        while ($parent = $reflectedClass->getParentClass()) {
+            $result[] = $parent;
+            $reflectedClass = $parent;
+        }
+
+        return $result;
+    }
+
+    /**
      * Returns the class under test via Reflection.
      *
      * This method is meant to be used in a class which extends
-     * `\PHPUnit_Framework_TestCase`.
+     * `PHPUnit_Framework_TestCase`.
      *
      * @param string $classSuffix The sufix of the class name, the convention
      *                            is `Test`.
@@ -98,7 +233,7 @@ class ReflectionUtils
      * visibility.
      *
      * This method is meant to be used in a class which extends
-     * `\PHPUnit_Framework_TestCase`.
+     * `PHPUnit_Framework_TestCase`.
      *
      * @param string $methodPrefix The prefix of the method name, the convention
      *                             is `test`.
